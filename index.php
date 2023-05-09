b22b1eeb4295d2a7d37cb53f69bea5068ab0f9d5
MartinaDR1 <derosemartina933@gmail.com>
2023-05-09 14:29:20 +0200

    refactoring

--- index.php
+++ index.php
@@ -3,21 +3,8 @@ session_start();
 
 ?>
 
+    <?php include __DIR__ . '/views/partials/layout/head.php'; ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="./css/style.css">
-    <title>PHP Strong Password Generator</title>
-</head>
-<body>
     <?php include __DIR__ . '/views/partials/header.php'; ?>
 
     <div class="container my-3">
--- result.php
+++ result.php
@@ -30,20 +30,8 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include __DIR__ . '/views/partials/layout/head.php'; ?>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="./css/style.css">
-
-    <title>Random password</title>
-</head>
-<body>
     <?php include __DIR__ . '/views/partials/header.php'; ?>
 
     <div class="container">
